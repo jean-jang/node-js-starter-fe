92794ee61a21b4fa0f9032bbba693eaec8d89702
Jean <jeanjyjh@gmail.com>
2024-10-22 21:40:06 +1300

    Add loading state to display loading screen during route changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,55 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import TodoPage from "./pages/TodoPage.js";
 import RegisterPage from "./pages/RegisterPage.js";
 import LoginPage from "./pages/LoginPage.js";
-import React from "react";
+import ProtectedRoute from "./routes/ProtectedRoute.js";
+import api from "./utils/api";
+import React, { useEffect } from "react";
 
 function App() {
-  return (
+  const [user, setUser] = React.useState(null);
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  const getUser = async () => {
+    const storedToken = sessionStorage.getItem("authToken");
+    try {
+      if (storedToken) {
+        const response = await api.get("/user/me");
+        setUser(response.data);
+      }
+    } catch (error) {
+      setUser(null);
+      console.error("Invalid token", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  return isLoading ? (
+    <div>Loading...</div>
+  ) : (
     <Router>
       <Routes>
-        <Route path="/" element={<TodoPage />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute user={user}>
+              <TodoPage />
+            </ProtectedRoute>
+          }
+        />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="/login"
+          element={<LoginPage setUser={setUser} user={user} />}
+        />
       </Routes>
     </Router>
   );
